85f8a6df310a8a196a0d35753d38a795808c6359
Jose Christiano <christiano.viana.junior@gmail.com>
2021-03-28 13:06:47 -0300

    appsettings

--- src/app/shared/appsetting.ts
+++ src/app/shared/appsetting.ts
@@ -1,5 +1,6 @@
 export class AppSetting{
-    static apiBaseUrl = 'http://localhost:5000/api/v1';
+    static apiBaseUrl = 'https://doasonhos-api.azurewebsites.net/api/v1'
+    //static apiBaseUrl = 'http://localhost:5000/api/v1';
     static ibgeApiUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades';
     static cepApiUrl  = 'https://viacep.com.br/ws/';
 }
